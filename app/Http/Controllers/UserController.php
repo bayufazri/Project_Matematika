a3e4b0f39baf9f9a67c52725bc1c3caa96e3c4d3
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-02-14 15:06:23 +0700

    fix controller & route

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
+//rizalihwan
 class UserController extends Controller
 {
     public function myProfile()
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('auth.login');
+})->middleware('guest');
 
 Auth::routes();
 
+//rizalihwan
 Route::get('/home', 'HomeController@index')->name('home');
 Route::prefix('profile')->name('profile.')->middleware('auth')->group(function () {
     Route::get('/myprofile', 'UserController@myProfile')->name('myprofile');
